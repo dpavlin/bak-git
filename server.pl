2b26bb1fff48e9a35db92bdf21409134528a66b3
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-02-17 23:49:26 +0100

    better output, fixed install

--- server.pl
+++ server.pl
@@ -7,7 +7,15 @@ only depenency) to ad-hoc remote server
 
 Install on client with:
 
-  echo install | nc 10.60.0.92 9001 > bak ; chmod 755 bak
+  echo install | nc 127.0.0.1 9001 > bak ; chmod 755 bak
+
+Start server with:
+
+  ./server.pl /path/to/backup 127.0.0.1
+
+You will want to add following to C<~/.ssh/config>
+
+  RemoteForward 9001 localhost:9001
 
 =cut
 
@@ -17,29 +25,45 @@ use autodie;
 use IO::Socket::INET;
 use File::Path;
 
-my $dir = shift @ARGV || die "usage: $0 /backup/directory\n";
+my ( $dir, $server_ip ) = @ARGV;
+die "usage: $0 /backup/directory\n" unless $dir;
+$server_ip ||= '127.0.0.1';
+
+my $shell_client = <<__SHELL_CLIENT__;
+#!/bin/sh
+echo `pwd` \$* | nc $server_ip 9001
+__SHELL_CLIENT__
 
 chdir $dir;
 system 'git init' unless -e '.git';
 
 my $server = IO::Socket::INET->new(
 	Proto     => 'tcp',
+	LocalAddr => $server_ip,
 	LocalPort => 9001,
 	Listen    => SOMAXCONN,
 	Reuse     => 1
 ) || die $!;
 
+
+warn "dir: $dir listen: $server_ip:9001\n"
+	, "remote-host> echo install | nc $server_ip 9001 > bak ; chmod 755 bak\n"
+	, $shell_client
+;
+
 while (my $client = $server->accept()) {
 	my ($pwd,$command,$path,$message) = split(/\s+/,<$client>,4);
 	my $ip = $client->peerhost;
 
-	$message ||= '';
-	$path = "$pwd/$path" unless $path =~ m{^/};
-
-	if ( $command eq 'install' ) {
-		print $client '#!/bin/sh',$/,'echo `pwd` $* | nc 10.60.0.92 9001',$/;
+	if ( $pwd eq 'install' ) {
+		warn "install on $ip\n";
+		print $client $shell_client;
+		close($client);
 		next;
 	}
+
+	$message ||= '';
+	$path = "$pwd/$path" unless $path =~ m{^/};
 
 	warn "$ip [$command] $path | $message\n";
 
