70b14626e1b1e1b9d0152f200bb8129b25e2abea
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-03-11 17:18:37 +0100

    start gitweb and tunnels only without args

--- klin.sh
+++ klin.sh
@@ -1,4 +1,13 @@
-#!/bin/sh -x
+ #!/bin/sh -x
+
+if [ -z "$1" ] ; then
 
 /usr/sbin/lighttpd -f gitweb/httpd.conf
+
+autossh -N -R 9001:10.60.0.92:9001 root@webgui &
+autossh -N -R 9001:10.60.0.92:9001 root@saturn.ffzg.hr &
+autossh -N asa-klin &
+
+fi
+
 ./bak-git-server.pl /home/dpavlin/klin/backup/ 10.60.0.92 $*
