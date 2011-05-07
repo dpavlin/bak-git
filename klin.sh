0011c35a5bce5c76219d960d3b941c43f30ae907
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-05-07 15:11:25 +0200

    no need to start autossh manually

--- klin.sh
+++ klin.sh
@@ -4,10 +4,6 @@ if [ -z "$1" ] ; then
 
 /usr/sbin/lighttpd -f gitweb/httpd.conf
 
-autossh -N -R 9001:10.60.0.92:9001 root@webgui &
-autossh -N -R 9001:10.60.0.92:9001 root@saturn.ffzg.hr &
-autossh -N asa-klin &
-
 fi
 
 ./bak-git-server.pl /home/dpavlin/klin/backup/ 10.60.0.92 $*
