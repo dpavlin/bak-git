71f520e40818e2075db15687aeed298e8fdff2cd
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-06-21 23:32:32 +0200

    use autossh for tunneling

--- klin.sh
+++ klin.sh
@@ -1,4 +1,4 @@
- #!/bin/sh -x
+#!/bin/sh -x
 
 if [ -z "$1" ] ; then
 
@@ -6,4 +6,4 @@ if [ -z "$1" ] ; then
 
 fi
 
-./bak-git-server.pl /home/dpavlin/klin/backup/ 10.60.0.92 $*
+SSH=autossh ./bak-git-server.pl /home/dpavlin/klin/backup/ 10.60.0.92 $*
